01a9f146b6f1c2f5ed6d496c4ca78a84bde4325b
Federico Paolinelli <fpaoline@redhat.com>
2023-09-27 04:02:23 +0200

    Add an optional "fieldOwner" field to set when injecting the caBundle (#118)
    * Add an optional "fieldOwner" field to set when injecting the caBundle
    
    When using the cert-rotator with CI/CD systems such as argoCD, the
    injection of the caBundle makes the cd system to reconcile it again. By
    setting the field owner, we instruct the cd system that the field
    is managed by an external component so it will ignore those updates.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>
    
    * Unit tests: validate the fieldowner field
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>
    
    ---------
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>
    Co-authored-by: Sertaç Özercan <852750+sozercan@users.noreply.github.com>
    Co-authored-by: Max Smythe <smythe@google.com>

--- pkg/rotator/rotator.go
+++ pkg/rotator/rotator.go
@@ -161,6 +161,7 @@ func AddRotator(mgr manager.Manager, cr *CertRotator) error {
 		webhooks:                    cr.Webhooks,
 		needLeaderElection:          cr.RequireLeaderElection,
 		refreshCertIfNeededDelegate: cr.refreshCertIfNeeded,
+		fieldOwner:                  cr.FieldOwner,
 	}
 	if err := addController(mgr, reconciler); err != nil {
 		return err
@@ -207,14 +208,16 @@ type CertRotator struct {
 	reader SyncingReader
 	writer client.Writer
 
-	SecretKey              types.NamespacedName
-	CertDir                string
-	CAName                 string
-	CAOrganization         string
-	DNSName                string
-	ExtraDNSNames          []string
-	IsReady                chan struct{}
-	Webhooks               []WebhookInfo
+	SecretKey      types.NamespacedName
+	CertDir        string
+	CAName         string
+	CAOrganization string
+	DNSName        string
+	ExtraDNSNames  []string
+	IsReady        chan struct{}
+	Webhooks       []WebhookInfo
+	// FieldOwner is the optional fieldmanager of the webhook updated fields.
+	FieldOwner             string
 	RestartOnSecretRefresh bool
 	ExtKeyUsages           *[]x509.ExtKeyUsage
 	// RequireLeaderElection should be set to true if the CertRotator needs to
@@ -723,6 +726,7 @@ type ReconcileWH struct {
 	wasCAInjected               *atomic.Bool
 	needLeaderElection          bool
 	refreshCertIfNeededDelegate func() (bool, error)
+	fieldOwner                  string
 }
 
 // Reconcile reads that state of the cluster for a validatingwebhookconfiguration
@@ -817,7 +821,11 @@ func (r *ReconcileWH) ensureCerts(certPem []byte) error {
 			anyError = err
 			continue
 		}
-		if err := r.writer.Update(r.ctx, updatedResource); err != nil {
+		opts := []client.UpdateOption{}
+		if r.fieldOwner != "" {
+			opts = append(opts, client.FieldOwner(r.fieldOwner))
+		}
+		if err := r.writer.Update(r.ctx, updatedResource, opts...); err != nil {
 			log.Error(err, "Error updating webhook with certificate")
 			anyError = err
 			continue
